77521225b2d11df4c8b1b75fd96211213f4da9ac
MadhuraBK26 <madhura.bk@compassitesinc.com>
2016-10-12 16:17:50 +0530

    Add files via upload

---
+++ MySQL_Basics2.php
@@ -0,0 +1,33 @@
+<?php
+
+$user_name = "root";
+$password  = "compass";
+$database  = "mytodo";
+$server    = "localhost";
+
+mysql_connect("$server", "$user_name", "$password");
+
+mysql_select_db("$database");
+
+
+$order = "INSERT INTO MyGuests
+
+          VALUES
+
+          ('madhura',
+
+           'udupi')";
+
+
+$result = mysql_query($order);
+
+if ($result) {
+    
+    echo ("<br>Input data is succeed");
+    
+} else {
+    
+    echo ("<br>Input data is fail");
+    
+}
+?> 
